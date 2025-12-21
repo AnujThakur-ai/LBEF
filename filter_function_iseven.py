202d75daf83ef92f702419aca0339f88213172ac
Anuj Thakur <anuj@localhost.localdomain>
2025-12-21 09:27:49 +0545

    changed filter_iseven.py

--- filter_function_iseven.py
+++ filter_function_iseven.py
@@ -3,4 +3,11 @@
 
 l = [1,2,3,11,22,44,112,453,123,55,345,645]
 l1 = filter(lambda x : x%2 ==0, l)
-print(list(l1))
+print("is even: ", list(l1))
+
+l2 = filter(lambda x : x%2 !=0, l)
+print("is odd: ", list(l2))
+
+l3 = list(filter(lambda X: X > 100, l))
+
+print("greater than 100: : ", l3)
